b87947ab3ff8fc4fdf5f34812b84578890e70a9d
asemencgt <asemencgt10@gmail.com>
2025-09-12 18:36:45 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -90,7 +90,7 @@ export default function HomePage() {
               className="w-full h-full rounded-lg shadow-lg"
               controls
             >
-              <source src="/videos/doc VH web.mp4" type="video/mp4" />
+              <source src="videos/doc VH web.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
