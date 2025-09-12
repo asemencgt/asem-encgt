0ba77f43be5e03a70687e9eab15a910864643fcb
asemencgt <asemencgt10@gmail.com>
2025-09-12 18:54:31 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -84,13 +84,13 @@ export default function HomePage() {
 </section>
 
       {/* Full Video Section with Corrected Aspect Ratio */}
-        <section className="bg-black py-12 flex justify-center items-center">
+        <section className="bg-black py-0 sm:py-12 flex justify-center items-center">
           <div className="relative w-full max-w-7xl mx-auto aspect-video">
             <video
               className="absolute inset-0 w-full h-full rounded-lg shadow-lg object-cover"
               controls
             >
-              <source src="videos/doc VH web.mp4" type="video/mp4" />
+              <source src="/videos/doc VH web.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
